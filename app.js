5710294c7c428f8846cc5f017839db768f928101
Tess Craig <tessashleycraig89@gmail.com>
2017-12-10 14:05:27 -0600

    Commit 8 - adds 'Developed by', defines players and movePlayer function, adds event listener keypress

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
+const player1 = document.getElementById('player1');
+const player2 = document.getElementById('player2');
 
+const movePlayer = () => {
+  document.addEventListener('keypress', (event)) => {
+    if (event.key === 'z') {
+
+    } else if (event.key === 'p') {
+      
+    }
+  }
+}
