f07d33c1425fcb3779efaea4c9b7f6a9b6aaa2d2
Tess Craig <tessashleycraig89@gmail.com>
2017-12-10 15:28:52 -0600

    Commit 8 - updated javascript plan

--- app.js
+++ app.js
@@ -6,7 +6,25 @@ const movePlayer = () => {
     if (event.key === 'z') {
 
     } else if (event.key === 'p') {
-      
+
     }
   }
 }
+
+//define player1 position
+//define player2 position
+//define final position
+//define player1 lane
+//define player2 lane
+
+//add addEventListener that triggers move function
+
+//move function
+// for (start position = 0; position <= lane.length; position++)
+    //
+
+//anounce function
+    //if player1 postion = final postion && player2 position !== final position
+        //alert player1 won
+    //else if player2 postion = final postion && player1 position !== final position
+        //alert player2 won
